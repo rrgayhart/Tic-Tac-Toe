d6f6dc91692936ccaa35181de75ad644f597b832
Meeka <rrgayhart@gmail.com>
2013-10-07 11:03:30 -0600

    Makes key command reading functional and begins drawing based on commands

--- tictactoe.rb
+++ tictactoe.rb
@@ -9,7 +9,47 @@ class TicTacToe < Processing::App
     line(box,2*third,0,2*third)
     line(third,box,third,0)
     line(2*third,box,2*third,0)
-    #line(x1,y1,x2,y2)
+  end
+
+  def draw
+  end
+
+  def key_pressed
+    warn " A key was pressed: #{key.inspect}"
+    @queue ||= ""
+    if key != "\n"
+      @queue += key
+    else
+      if @queue =~ /(X|O)\d/i
+        run_command(@queue)
+      else
+        warn "The command should be an X or an O followed by 1-9."
+      end
+      @queue = ""
+    end
+  end
+
+  def run_command(input)
+    number = input[-1]
+    letter = input[0]
+    place_mark_in_box(number, letter)
+    warn "You made a play"
+  end
+
+  def place_mark_in_box(number, letter)
+    # figure out, x, y coordinates of where to put the shapes
+    if number == 1
+      x_coord = 0
+      y_coord = 0
+    end
+
+    if letter == 120
+      fill 0,20
+      rect(x_coord,y_coord,50,50)
+    else letter == 111
+      fill 0,200
+      rect(x_coord,y_coord,50,50)
+    end
   end
 
   box = 200
