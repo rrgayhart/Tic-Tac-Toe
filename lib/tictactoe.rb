94c5bb72c1bbfd7c30131c9d0c5441ea3358ff02
Simon Taranto <simon.taranto@gmail.com>
2013-10-10 15:58:51 -0600

    get initial server calls working

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,7 +1,8 @@
 require 'ruby-processing'
-require 'board'
-require 'boardview'
-
+require './board'
+require './boardview'
+require 'httparty'
+require 'json'
 class TicTacToe < Processing::App
 
   attr_reader :board_view, :board, :box, :box_dimension
@@ -11,6 +12,31 @@ class TicTacToe < Processing::App
     @board_view = BoardView.new(self, @board)
     @box = 200
     @box_dimension = 3
+    #@thread = Thread.new do
+    #  while true do
+    #    puts "checking for move, fool"
+    #    sleep 5
+    #  end
+    #end
+    @game_id = create_game
+    puts "game id is #{game_id}"
+    joined_status = join_game(game_id)
+    puts "after joining"
+    puts "Game #{game_id} joined" if joined_status
+  end
+
+  attr_reader :game_id
+
+  def create_game
+    response = HTTParty.post("http://safe-brook-1547.herokuapp.com/games", { :query => {:player_one => "Thomas"}})
+    data = JSON.parse(response.body)
+    data["id"]
+  end
+
+  def join_game(game_id)
+    response = HTTParty.put("http://safe-brook-1547.herokuapp.com/games/#{game_id}", { :query => {:player_two => "Engine"}})
+    data = JSON.parse(response.body)
+    data["joined"]
   end
 
   # def draw_winning_line(fx,fy,lx,ly)
@@ -27,8 +53,35 @@ class TicTacToe < Processing::App
     @player_marker ||= 'x'
     letter_mapping = (@player_marker == 'x' ? 1 : 0)
     placement_mapping = {1 => :a, 2 => :b, 3 => :c, 4 => :d, 5 => :e, 6 => :f, 7 => :g, 8 => :h, 9 => :i}
-    board.move(placement_mapping[box_num], letter_mapping) unless board.expired?
-    @player_marker = @player_marker == 'o' ? 'x' : 'o'
+    box_num_state = board.spaces[placement_mapping[box_num]].nil?
+
+    row_col = create_row_col_from_box_num(box_num)
+    row = row_col[1]
+    col = row_col[0]
+    web_board_position = "#{col},#{row},#{@player_marker}"
+
+    HTTParty.post("http://safe-brook-1547.herokuapp.com/games/#{game_id}/moves", { :query => { :data => web_board_position } })
+    #board.move(placement_mapping[box_num], letter_mapping) unless board.expired?
+    response = HTTParty.get("http://safe-brook-1547.herokuapp.com/games/#{game_id}/moves/last")
+    puts "last move is #{response}"
+    data = JSON.parse(response.body)["move"]
+    x,y,letter = data.split(",")
+    puts "x is #{x}"
+    puts "y is #{y}"
+    web_letter_mapping = (letter == 'x' ? 1 : 0)
+    web_box_number = determine_box(x.to_i,y.to_i)
+    puts "web_letter is #{web_letter_mapping}"
+    puts "web_box is #{web_box_number}"
+    board.move(placement_mapping[web_box_number], web_letter_mapping)
+    if box_num_state
+      @player_marker = @player_marker == 'o' ? 'x' : 'o'
+    end
+  end
+
+  def create_row_col_from_box_num(box_num)
+    row = (box_num/box_dimension.to_f).ceil
+    column = box_num%box_dimension == 0 ? box_dimension : box_num%box_dimension
+    [column, row]
   end
 
   def determine_column_or_row(position)
